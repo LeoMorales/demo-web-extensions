21141686a991c20178ad60d8f1e7cf6b2d89d2b4
LeoMorales <leo.moralesr23@gmail.com>
2018-09-26 17:16:47 -0300

    Commit inicial

---
+++ src/content_scripts/main.js
@@ -0,0 +1,6 @@
+nlp = require('compromise');
+
+//document.querySelector("body").style["background-color"] = "yellow";
+document.body.style.border = "5px solid yellow";
+
+console.log("web extension!");
